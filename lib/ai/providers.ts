404e4be8edd2321bce6dcbdee22112d50e8ea341
conrado.guerra <conrado.guerra@ilek.fr>
2025-08-24 16:10:32 +0200

    Add gemini as the model

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -2,36 +2,35 @@ import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
-} from 'ai';
-import { xai } from '@ai-sdk/xai';
+} from "ai";
+import { google } from "@ai-sdk/google";
 import {
   artifactModel,
   chatModel,
   reasoningModel,
   titleModel,
-} from './models.test';
-import { isTestEnvironment } from '../constants';
+} from "./models.test";
+import { isTestEnvironment } from "../constants";
+
+const model = google('gemini-2.5-flash');
 
 export const myProvider = isTestEnvironment
   ? customProvider({
       languageModels: {
-        'chat-model': chatModel,
-        'chat-model-reasoning': reasoningModel,
-        'title-model': titleModel,
-        'artifact-model': artifactModel,
+        "chat-model": chatModel,
+        "chat-model-reasoning": reasoningModel,
+        "title-model": titleModel,
+        "artifact-model": artifactModel,
       },
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
-        'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        "chat-model": model,
+        "chat-model-reasoning": wrapLanguageModel({
+          model: model,
+          middleware: extractReasoningMiddleware({ tagName: "think" }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
-      },
-      imageModels: {
-        'small-model': xai.imageModel('grok-2-image'),
+        "title-model": model,
+        "artifact-model": model,
       },
     });
