583dd983e9f07ab5eda586ecdaa0ecf582aa97c2
conrado.guerra <conrado.guerra@ilek.fr>
2025-08-27 14:05:00 +0200

    Add env variable redis

--- lib/infrastructure/cache/redis.client.ts
+++ lib/infrastructure/cache/redis.client.ts
@@ -2,12 +2,13 @@ import {
   createClient,
   type RedisClientType,
   type RedisDefaultModules,
-} from 'redis';
+} from "redis";
 
 export class RedisClient {
   private client: RedisClientType<RedisDefaultModules>;
 
-  constructor(url: string = 'redis://127.0.0.1:6380') {
+  constructor(url: string = process.env.REDIS_URL || "redis://127.0.0.1:6380") {
+    console.log(url)
     this.client = createClient({ url });
     this.client.connect();
   }
